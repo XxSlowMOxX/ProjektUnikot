82afb1c3eb7150ff255391cd2966c9458d4d82f7
wildug <wildug@LAPTOP-E71BIOP5.localdomain>
2020-12-19 14:32:34 +0100

    dummesGame gelöscht weil es dumm war, epileptikerFreundlich escapable gemacht

--- epileptikerFreundlich.py
+++ epileptikerFreundlich.py
@@ -1,12 +1,13 @@
-import os, random, kbhit, curses
-import threading, socket
+import os, random,  curses
+import  socket
+
 
 room = []
 players = []
 last_message = ""
 
 def readLevel(name):
-    loc = os.path.join(os.path.dirname(__file__), "levels/" + name.decode() +".map")
+    loc = os.path.join(os.path.dirname(__file__), "levels/" + name +".map")
     with open(loc, "r+") as mapfile:
         level = mapfile.read().split("\n")
         mapfile.close()
@@ -30,7 +31,7 @@ def hostServer(name):
         print("received message: %s" % data)
 
 class Player:
-    rep = "v"
+    rep = ">"
     def __init__(self, sx, sy, sid, col):
         self.x = sx
         self.y = sy
@@ -42,35 +43,41 @@ class Player:
             self.x += vx
             self.y += vy
 
-myPlayer = Player(1,1,random.randint(0, 10000),curses.COLOR_RED)
-players.append(myPlayer)            
-stdscr = curses.initscr()
-curses.cbreak()
-stdscr.keypad(True)
-stdscr.addstr(0,0,"These Maps are available: \n" + "\n".join(printLevels())) #new lvlselect
-stdscr.refresh()
-room = readLevel(stdscr.getstr(len(printLevels())+2,0, 5)) #end of lvlselect
-stdscr.clear()
-stdscr.nodelay(True)
-while True:
-    stdscr.addstr(0,0,"\n".join(room))
-    stdscr.addstr(myPlayer.x,myPlayer.y,myPlayer.rep)
-    kp = stdscr.getch()
-    if(kp == 3):
-        exit()  
-    elif(kp==ord("d")):
-        myPlayer.move(0,1,room)
-        myPlayer.rep = ">"
-    elif(kp==ord("a")):
-        myPlayer.move(0,-1,room)
-        myPlayer.rep = "<"
-    elif(kp==ord("w")):
-        myPlayer.move(-1,0,room)
-        myPlayer.rep = "ʌ"
-    elif(kp==ord("s")):
-        myPlayer.move(1,0,room)
-        myPlayer.rep = "v"
-        
+
+
+
+def rungame(stdscr):
+    myPlayer = Player(1,1,random.randint(0, 10000),curses.COLOR_RED)
+    players.append(myPlayer)            
+    curses.cbreak()
+    stdscr.keypad(True)
+    stdscr.nodelay(True)
+
+
+    stdscr.addstr(0,0,"These Maps are available: \n" + "\n".join(printLevels())) #new lvlselect
     stdscr.refresh()
-stdscr.refresh()
-stdscr.getkey()
+    room = readLevel('test') #end of lvlselect
+    stdscr.clear()
+    while True:
+        kp = stdscr.getch()
+        if(kp!=-1):
+            stdscr.addstr(0,0,"\n".join(room))
+            if(kp == 113):
+                exit()  
+            elif(kp==ord("d")):
+                myPlayer.move(0,1,room)
+                myPlayer.rep = ">"
+            elif(kp==ord("a")):
+                myPlayer.move(0,-1,room)
+                myPlayer.rep = "<"
+            elif(kp==ord("w")):
+                myPlayer.move(-1,0,room)
+                myPlayer.rep = "ʌ"
+            elif(kp==ord("s")):
+                myPlayer.move(1,0,room)
+                myPlayer.rep = "v"    
+            stdscr.addstr(myPlayer.x,myPlayer.y,myPlayer.rep)
+            
+            stdscr.refresh()
+
+curses.wrapper(rungame)
