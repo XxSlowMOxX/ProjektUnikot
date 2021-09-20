b7aedf8f229cbf54de94064f9a8eb7be1832de5b
SlowMO_Desktop <moritz-schuetz@gmx.de>
2021-09-20 23:01:07 +0200

    GitHub Fix

--- netzwerker.py
+++ netzwerker.py
@@ -1,33 +1,70 @@
-import socket, threading, netHelper, socketserver
+import socket, threading, netHelper, socketserver, time
+from gameclass import Game 
+import ast # for list <-> string parsing
+
 
 messages = []
-address = ("",4230)
+HOST_IP = ""
+MSG_T = 0.2
+PING_MSG = "PING"
+last_ping = 0
 
 def hostServer(name):
-	srvr = socketserver.UDPServer((netHelper.getIP(), 4230), UDPHandler)
-	srvr.serve_forever()
+    srvr = socketserver.UDPServer((netHelper.getIP(), 30814), UDPHandler)
+    srvr.serve_forever()
 
-def sendMessage(name, msg):
-	print(address)
+
+def hooker(name, HOST_IP, PORT, GAME):
+
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.sendto(msg.encode(),address)
-	print(str(sock.recv(1024), "utf-8"))
+
+	mpos = [] 
+	if (mpos != GAME.getMPos()): 
+################# FEHLT ################
+	    mpos = GAME.getMPos()
+################# FEHLT ################
+	    sock.sendto(str(mpos).encode(), (HOST_IP, PORT))
+
+	else:
+		sock.sendto(PING_MSG.encode(), (HOST_IP, PORT))
+	print(mpos)
+	print("sent")
+	opos =sock.recv(1024)
+
+################# FEHLT ################
+	GAME.setOPos(opos)
+################# FEHLT ################
+
+	time.sleep(MSG_T)
 
 class UDPHandler(socketserver.BaseRequestHandler):
-	def handle(self):
-		data = self.request.recv(1024).strip()
-		sock = self.request[1]
-		print(self.client_address[0] + " : " + data)
-		sock.sendto(data.upper(), self.client_address)
+    def handle(self):
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((netHelper.getIP(), 4230))
-print("Chat Client Starting")
-RCV_IP = input("Enter IP to connect to: ")
-address = (RCV_IP, 4230)
-listener = threading.Thread(target=hostServer, args=(1,),daemon=True)
-listener.start()
-while True:
-	MSG = input("MESSAGE: ")
-	sender = threading.Thread(target=sendMessage,args=(2,MSG),daemon=True)
-	sender.start()
+        data = self.request[0]
+        sock = self.request[1]
+        global last_ping
+        if(str(data, "utf-8").lower() == PING_MSG.lower()):
+            #print("RCVD")
+            if(len(messages) != 0):
+                sock.sendto(messages.pop(0).encode(), self.client_address)
+            else:
+                sock.sendto(str("Ping-ack").encode(), self.client_address)
+        else:
+            if(len(messages) != 0):
+                sock.sendto(messages.pop(0).encode(), self.client_address)
+            else:
+                sock.sendto(str("ack").encode(), self.client_address)
+            print("Received: " + str(data, "utf-8") + " and responded")
+
+def getHookedorListen(GAME, isClient=True, IP="", PORT=30814,):
+	""" returns thread to do stuff"""
+	if(not isClient):
+		isHost = True
+		print("Server Mode Selected. Starting now...")
+		listener = threading.Thread(target=hostServer, args=(1,), daemon=True)
+		listener.start()
+
+	else:
+		print("Connecting to %s:%d"% (IP, PORT))
+		hook = threading.Thread(target=hooker, args=(2, IP, PORT, GAME), daemon=True)
+		hook.start()
