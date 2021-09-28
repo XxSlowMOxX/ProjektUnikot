b56030c5e79285e82c5a7073b87ac783b3740969
Moritz Schütz <moritz-schuetz@gmx.de>
2021-09-28 23:21:21 +0200

    alternatives Game / Networker
    -Gitignores für neues PyCharm

---
+++ moGame.py
@@ -0,0 +1,120 @@
+import os
+import random  # random shit
+import curses  # rendering
+import socket, threading, netHelper  # helper files; get you IP
+import Netzwerker  # getHookedorListen function
+from gameclass import Game, Player
+import netHelper  # helper files
+import Menu  # Menu for Main Menu, etc.
+import time
+
+room = []
+players = []
+last_message = ""
+
+
+def readLevel(name):
+    loc = os.path.join(os.path.dirname(__file__), "levels/" + name + ".map")
+    with open(loc, "r+") as mapfile:
+        level = mapfile.read().split("\n")
+        mapfile.close()
+    return level[:]
+
+
+def printLevels():
+    arr = os.listdir(os.path.join(os.path.dirname(__file__), "levels/"))
+    mplist = []
+    for potMap in arr:
+        if(len(potMap) > 5 and potMap[-4:] == ".map"):
+            mplist.append(potMap)
+    return mplist
+
+
+def hostServer(name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((netHelper.getIP(), 4230))
+    global last_message
+    while True:
+        data, addr = sock.recvfrom(1024)
+        last_message = str(data) + "from" + str(addr)
+        print("received message: %s" % data)
+
+
+
+
+def rungame(stdscr):
+    myPlayer = Player(1, 1, random.randint(0, 10000), curses.COLOR_RED)
+    # own Player always FIRST Player, was btw ne blöde Idee ist
+    GAME = Game([myPlayer])
+    curses.cbreak()
+    stdscr.keypad(True)
+    stdscr.nodelay(True)
+
+    mainMenu = Menu.menu(["Singleplayer", "Multiplayer"], stdscr)
+
+    if (mainMenu == Menu.Singleplayer):
+        exit()
+
+    elif (mainMenu == Menu.Multiplayer):
+        mpMode = Menu.menu(["Host", "Join"], stdscr)
+
+        if (mpMode == Menu.Host):
+            Netzwerker.getHookedorListen(
+                    GAME, isClient=False, IP="localhost", PORT=30814)
+
+        elif (mpMode == Menu.Join):
+            myPlayer.x +=1
+            Netzwerker.getHookedorListen(
+                GAME, isClient=True, IP="localhost", PORT=30814)
+
+
+    levelIndex = Menu.menu(printLevels(), stdscr)
+    room = readLevel(printLevels()[levelIndex][:-4])
+
+    stdscr.clear()
+    curses.curs_set(0)
+    stdscr.addstr(0, 0, "\n".join(room))
+    for player in GAME.Players:
+        stdscr.addstr(player.x, player.y, player.rep)
+
+    stdscr.refresh()
+    stdscr.nodelay(True)
+
+    while True:
+        kp = stdscr.getch()
+        # DEBUGgging
+        stdscr.addstr(13,1,str(time.time()))
+
+
+
+        stdscr.addstr(0, 0, "\n".join(room))
+
+        for player in range(len(GAME.Players)):
+            # yes this is ugly
+            stdscr.addstr(GAME.Players[player].x, GAME.Players[player].y, "-")
+
+        if (kp == 113 or kp == 3):
+            exit()
+        elif (kp == ord("d")):
+            myPlayer.move(0, 1, room)
+            myPlayer.rep = ">"
+        elif (kp == ord("a")):
+            myPlayer.move(0, -1, room)
+            myPlayer.rep = "<"
+        elif (kp == ord("w")):
+            myPlayer.move(-1, 0, room)
+            myPlayer.rep = "ʌ"
+        elif (kp == ord("s")):
+            myPlayer.move(1, 0, room)
+            myPlayer.rep = "v"
+
+        for player in range(len(GAME.Players)):
+            # yes this is ugly
+            stdscr.addstr(10+player,1, str(GAME.Players[player].x)+
+                    ","+str(GAME.Players[player].y)+" "+GAME.Players[player].rep)
+            stdscr.addstr(GAME.Players[player].x, GAME.Players[player].y, GAME.Players[player].rep)
+
+        stdscr.refresh()
+
+
+curses.wrapper(rungame)
---
+++ networker.py
@@ -0,0 +1,74 @@
+import socket
+import threading
+import netHelper
+import socketserver
+import time
+
+messages = {}
+connections = []
+
+MSG_TIME = 0.2
+
+def hooker(name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    global messages
+    while True:
+        if (len(messages["192.168.178.78"]) != 0):
+            sock.sendto("#".join(messages[connections[0]]).encode(), (connections[0], 30814))
+            messages[connections[0]].clear()
+            print("sent")
+            print(sock.recv(4096))
+        #for cmd in ret.split("#"):
+         #   cmdHandler(cmd)
+        time.sleep(MSG_TIME)
+
+
+def hostServer(name):
+    srvr = socketserver.UDPServer((netHelper.getIP(), 30814), UDPHandler)
+    srvr.serve_forever()
+
+class UDPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+
+        data = self.request[0]
+        sock = self.request[1]
+
+        print(self.client_address[0] + " : " + str(data, "utf-8"))
+
+        if self.client_address[0] not in messages:
+            connections.append(self.client_address[0])
+            messages[self.client_address[0]] = []
+
+        for connection in connections:
+            if (connection != self.client_address[0]):
+                messages[connection].append(str(data, "utf-8"))
+
+        print(messages)
+        sock.sendto(str("empty#").encode(), self.client_address)
+
+
+
+def host():
+    print("ServerMode:")
+    listener = threading.Thread(target=hostServer, args=(1,), daemon=True)
+    listener.start()
+
+def client(HOST_IP):
+    print("ClientMode")
+    connections.append(HOST_IP)
+    hook = threading.Thread(target=hooker, args=(2,),daemon=True)
+    hook.start()
+
+def cmdHandler(cmd):
+    print(cmd)
+
+m = input("Mode")
+if (m == "h"):
+    host()
+    while True:
+        a = 1
+else:
+    client("192.168.178.78")
+    messages["192.168.178.78"] = []
+    while True:
+        messages["192.168.178.78"].append(input("M:"))
